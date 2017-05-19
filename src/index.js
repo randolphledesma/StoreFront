3de04fd9abf2aaee40d7d3585575d728b69eaa76
Randolph Ledesma <randolphl@zylun.com>
2017-05-19 14:36:34 +0800

    Test adding react-native-parallax-scroll-view component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+
 import Home from "./components/Home/index";
+
 export default class StoreFront extends Component {
   constructor() {
     super();
     this.state = {
       isReady: true
     };
-  }  
-  render() {    
+  }
+  render() {
     return <Home />;
   }
 }
